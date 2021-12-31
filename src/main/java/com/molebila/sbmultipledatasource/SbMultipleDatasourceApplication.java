665d1a3ff95ee685b229f7bd476897c42e6a2653
nurvadli <n.molebila@teravintech.com>
2021-12-31 14:25:54 +0700

    initial commit

---
+++ src/main/java/com/molebila/sbmultipledatasource/SbMultipleDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.molebila.sbmultipledatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMultipleDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbMultipleDatasourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/molebila/sbmultipledatasource/SbMultipleDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molebila.sbmultipledatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMultipleDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
